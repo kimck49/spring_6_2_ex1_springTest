de961eff92bf2cf14e87ffff59a9f796c2d90558
kimck49 <kimck49@naver.com>
2022-05-10 10:14:04 +0900

    자바파일로 컨테이너 설정파일 만들기

---
+++ src/main/java/com/gyojincompany/spring_6_2/MainClass.java
@@ -0,0 +1,30 @@
+package com.gyojincompany.spring_6_2;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		
+		Student stu1 = ctx.getBean("student1", Student.class);
+		
+		System.out.println(stu1.getName());
+		System.out.println(stu1.getAge());
+		System.out.println(stu1.getHobbys());
+		System.out.println(stu1.getHeight());
+		System.out.println(stu1.getWeight());
+		
+		Student stu2 = ctx.getBean("student2", Student.class);
+		
+		System.out.println(stu2.getName());
+		System.out.println(stu2.getAge());
+		System.out.println(stu2.getHobbys());
+		System.out.println(stu2.getHeight());
+		System.out.println(stu2.getWeight());
+		
+	}
+
+}
